a0152563260f9bdd94c113dc9acf97f5a3759f0e
ioann <ivan.karmalyk2017@gmail.com>
2022-12-06 17:17:39 +0200

    prototype pattern

---
+++ creational/prototype.php
@@ -0,0 +1,53 @@
+<?php
+
+abstract class WorkerPrototype
+{
+    protected string $name;
+    protected string $position;
+
+    /**
+     * @return string
+     */
+    public function getPosition(): string
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param string $position
+     */
+    public function setPosition(string $position): void
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
+
+class Developer extends WorkerPrototype
+{
+    protected string $position = "Developer";
+}
+
+$developer = new Developer();
+$developer2 = clone $developer;
+$developer3 = clone $developer;
+$developer2->setName('Ioann');
+$developer3->setName('Alex');
+
+var_dump($developer2->getName());
+var_dump($developer3->getName());
